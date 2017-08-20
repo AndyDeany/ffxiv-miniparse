2485d35deb6478adaf4d570aef26a2ea8bad5227
AndyDeany <oneandydean@hotmail.com>
2017-08-20 18:26:07 +0100

    Move config order to make more sense

--- config.js
+++ config.js
@@ -3,17 +3,6 @@ const PLAYER_NAME = "Sara Neko";
 const PARTY_MAX_HIT_TITLE = "Master Hit";
 const PLAYER_MAX_HIT_TITLE = "Kitt Hit";
 
-// set to 1 if you don't want to include LBs as a party max hit
-var noLBInPartyMaxHit = 1;
-
-// set to 1 if you don't want your own max hit in the party max hit.
-// ie: the party max hit will show the max of your party members minus you
-var noMyMaxHitInPartyMaxHit = 1;
-
-// set to 1 to highlight the higher max hit between user and party
-// only does something if both {maxHitCustom} and {myMaxHitCustom} are in encounterDefine
-var colorHigherMaxHit = 1;
-
 function blueText(text) {
   return "<span class='enc'>" + text + "</span>";
 }
@@ -24,3 +13,15 @@ var encounterDefine = `
 `
 
 var useHTMLEncounterDefine = true;  // Must be true for encounterDefine to render HTML
+
+
+// set to 1 if you don't want to include LBs as a party max hit
+var noLBInPartyMaxHit = 1;
+
+// set to 1 if you don't want your own max hit in the party max hit.
+// ie: the party max hit will show the max of your party members minus you
+var noMyMaxHitInPartyMaxHit = 1;
+
+// set to 1 to highlight the higher max hit between user and party
+// only does something if both {maxHitCustom} and {myMaxHitCustom} are in encounterDefine
+var colorHigherMaxHit = 1;
